8ae278fa53ef81709a404a9c1bbe76575d110411
ShinjungOh <tlswjd9260@gmail.com>
2023-01-02 23:54:26 +0900

    feat: 투두리스트 API 구현, todo interface 분리

---
+++ src/lib/apis/todoItemApi.ts
@@ -0,0 +1,36 @@
+import { client } from './client';
+
+export const createTodo = async (todo: string) => {
+  const url = '/todos';
+  const data = {
+    todo,
+  };
+  const response = await client.post(url, data);
+  return response;
+};
+
+export const getTodos = async () => {
+  const url = '/todos';
+  const response = await client.get(url);
+  return response;
+};
+
+export const updateTodo = async (
+  id?: number,
+  todo?: string,
+  isCompleted?: boolean,
+) => {
+  const url = `/todos/:${id}`;
+  const data = {
+    todo,
+    isCompleted,
+  };
+  const response = await client.put(url, data);
+  return response;
+};
+
+export const deleteTodo = async (id: number) => {
+  const url = `/todos/:${id}`;
+  const response = await client.delete(url);
+  return response;
+};
---
+++ src/lib/types/todoItemProps.ts
@@ -0,0 +1,5 @@
+export interface TodoProps {
+  id: number;
+  isCompleted: boolean;
+  text: string;
+}
